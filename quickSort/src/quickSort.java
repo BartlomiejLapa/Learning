534b09fc31cc60cc6d6d912088cfd938e0402ff8
spinaczel <spinaczel@o2.pl>
2018-02-11 22:41:01 +0100

    asd

--- quickSort/src/quickSort.java
+++ quickSort/src/quickSort.java
@@ -2,7 +2,7 @@ import java.util.Random;
 public class quickSort {
     public static void main (String args[]){
 
-        int array[] = new int[9];
+/*        int array[] = new int[9];
         Random random = new Random();
 
         System.out.println("Tablica nieposrtowana: ");
@@ -11,12 +11,53 @@ public class quickSort {
             System.out.print(array[i]);
             if (i == array.length-1) continue;
             System.out.print(", ");
+        }*/
+
+    int array[] = {14, 17, 5, 0, 11, 12, 8, 2, 5, 3, 4};
+
+        System.out.println("\nTablica nieposortowana: ");
+        for (int i = 0; i < array.length; i++){
+            System.out.print(array[i]);
+            if (i == array.length-1) continue;
+            System.out.print(", ");
         }
 
     //quicksort
 
         //znalezienie pivota
-        System.out.println("\nWartosc Pivota: " + array[array.length/2] + "\nindex tablicy: [" + array.length/2 + "]");
+        int pivot = array[array.length/2];
+        int pivotIndex = array.length/2;
+        System.out.println("\nWartosc Pivota: " + pivot + "\nindex tablicy: [" + pivotIndex + "]");
+
+        //szukanie wiekszej liczby po lewej stronie
+        int l,r;
+        int z = array.length-1;
+       quickS: for (int i=0; i < pivotIndex; i++){
+            if (array[i] < pivot) continue;
+            l = array[i];
+
+        //gdy znajdzie wieksza liczbe po stronie lewej szuka liczby mniejszej po stronie prawej od pivota
+            for (int j=z; j >= pivotIndex ; j--) {
+                if (array[j] >= pivot) continue;
+                r = array[j];
+                array[i] = r;
+                array[j] = l;
+
+                //bedzie zaczynac ponownie petle od pozycji o 1 mniejszej
+                z = j - 1;
+                continue quickS;
+
+
+            }
+
+        }
+
+        System.out.println("\nTablica posrtowana: ");
+        for (int i = 0; i < array.length; i++){
+            System.out.print(array[i]);
+            if (i == array.length-1) continue;
+            System.out.print(", ");
+        }
 
     }
 }
