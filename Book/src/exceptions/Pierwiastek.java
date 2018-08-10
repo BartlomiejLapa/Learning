d36570e9374bb511ead0a098c041e5d0cf34eb92
spinaczel <spinaczel@o2.pl>
2018-08-10 10:38:59 +0200

    asd

---
+++ Book/src/exceptions/Pierwiastek.java
@@ -0,0 +1,32 @@
+package exceptions;
+import java.lang.Math.*;
+import java.util.Scanner;
+
+class Pierwiastkowanie {
+    static int result;
+
+    static void sqrtNumber(int number) {
+        result = (int) Math.sqrt(number);
+        System.out.println("Pierwiastek z " + number + " wynosi " + result);
+
+    }
+
+}
+
+public class Pierwiastek {
+    public static void main(String args[]) throws java.io.IOException {
+        int odczytNumber;
+        Scanner odczyt = new Scanner(System.in);
+        System.out.println("Podaj liczbe do pierwiastkowania");
+        try {
+            odczytNumber = odczyt.nextInt();
+            if (odczytNumber < 0)
+                throw new IllegalArgumentException("Nie ma pierwiastka z liczb ujemnych(chuja prawda)");
+            Pierwiastkowanie.sqrtNumber(odczytNumber);
+        } catch (java.util.InputMismatchException exc) {
+            System.out.println("Podano niewlasciwy znak");
+        }
+
+    }
+
+}
