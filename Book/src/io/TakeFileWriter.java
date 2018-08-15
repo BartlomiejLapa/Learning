d258082f4176aa43352a29633adc869144213e56
spinaczel <spinaczel@o2.pl>
2018-08-15 18:50:08 +0200

    asd

--- Book/src/io/TakeFileWriter.java
+++ Book/src/io/TakeFileWriter.java
@@ -1,20 +1,23 @@
 package io;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
-class TakeWriter {
+class TakeWriter  {
     private String file;
     private String path = "C:\\Users\\bluefusion\\IdeaProjects\\Learning\\Book\\src\\io\\";
     String message;
-    String pathFile = "C:\\Users\\bluefusion\\IdeaProjects\\Learning\\Book\\src\\io\\file3.txt";
+    String pathFile;
     BufferedWriter saving = null;
 
+    TakeWriter() throws IOException {
+    }
 
 
-/*    void setPathFile() throws IOException {
+    void setPathFile() throws IOException {
         System.out.println("Podaj nazwe i rozszerzenei pliku");
         Scanner reading = new Scanner(System.in);
         file = reading.nextLine();
@@ -28,30 +31,40 @@ class TakeWriter {
                 saving.close();
             }
         }
-    }*/
+        System.out.print(pathFile);
+    }
 
     void getMessage() throws IOException {
 
         try {
+
+
             Scanner reading = new Scanner(System.in);
-            BufferedWriter saving = new BufferedWriter(new FileWriter(pathFile, true));
+            System.out.print("#### Aby wyczyscic plik wpisz \"null\" ####\n");
         do {
-            System.out.println("Podaj wiadomosc do zapisania, w celu zatrzymania wcisnij \"-\" ");
+            System.out.println("Podaj wiadomosc do zapisania, w celu zatrzymania zostaw puste pole i wcisnij enter.");
+            BufferedWriter saving = new BufferedWriter(new FileWriter(pathFile, true));
             message = reading.nextLine();
             saving.write(message);
+            if (message.equals("null")) clearFile();
             saving.newLine();
             saving.close();
 
-        } while (!message.equals("-"));
+
+        } while (!message.equals(""));
 
 
         } finally {
             if (saving != null) {
                 saving.close();
-
-
+                saving.flush();
             }
         }
+    }
+
+    void clearFile() throws IOException {
+        FileWriter clear = new FileWriter(pathFile);
+        clear.write(toString());
     }
 }
 
@@ -63,7 +76,7 @@ public class TakeFileWriter {
 
         TakeWriter start = new TakeWriter();
 
-//        start.setPathFile();
+        start.setPathFile();
         start.getMessage();
 
 
