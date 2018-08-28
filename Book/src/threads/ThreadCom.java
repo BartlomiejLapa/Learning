b1ba54db4f113cdd508858f2602fcae96640a427
spinaczel <spinaczel@o2.pl>
2018-08-28 15:49:25 +0200

    asd

---
+++ Book/src/threads/ThreadCom.java
@@ -0,0 +1,84 @@
+package threads;
+
+class TicTock {
+    String state;
+
+    synchronized void tick(boolean running) {
+        if (!running) {
+            state = "ticked";
+            notify();
+            return;
+        }
+
+        System.out.print("tik ");
+        state = "ticked";
+        notify();
+
+        try {
+            while (!state.equals("tocked")) {
+                wait();
+            }
+        } catch (InterruptedException exc) {
+            System.out.println("Wątek został przerwany.");
+        }
+
+    }
+
+    synchronized void tock(boolean running) {
+        if (!running) {
+            state = "tocked";
+            notify();
+            return;
+        }
+
+        System.out.println("tock");
+        state = "tocked";
+        notify();
+
+        try {
+            while (!state.equals("ticked")) {
+                wait();
+            }
+        } catch (InterruptedException exc) {
+            System.out.println("Wątek przerwany zosatał");
+        }
+    }
+}
+
+class MyThreadCom implements Runnable{
+    Thread thrd;
+    TicTock tt0b;
+
+    MyThreadCom(String name, TicTock tt) {
+        thrd = new Thread(this, name);
+        tt0b = tt;
+        thrd.start();
+
+    }
+
+    public void run() {
+
+        if (thrd.getName().compareTo("tick") == 0) {
+            for (int i = 0; i < 5; i++) tt0b.tick(true);
+            tt0b.tick(false);
+
+        } else {
+            for (int i = 0; i < 5; i++) tt0b.tock(true);
+            tt0b.tock(false);
+        }
+    }
+}
+public class ThreadCom {
+    public static void main(String args[]) {
+        TicTock tt = new TicTock();
+        MyThreadCom mt1 = new MyThreadCom("tick", tt);
+        MyThreadCom mt2 = new MyThreadCom("tock", tt);
+
+        try {
+            mt1.thrd.join();
+            mt2.thrd.join();
+        } catch (InterruptedException exc) {
+            System.out.println("Wątek główny został przerwany.");
+        }
+    }
+}
