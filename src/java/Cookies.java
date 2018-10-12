5b27a8f2d268da3990bc7f8897ad6ec8bb40df2e
spinaczel <spinaczel@o2.pl>
2018-10-12 15:29:01 +0200

    asd

--- src/java/Cookies.java
+++ src/java/Cookies.java
@@ -21,15 +21,7 @@ import javax.servlet.http.Cookie;
 @WebServlet(name = "Cookies", urlPatterns = {"/Cookies.do"})
 public class Cookies extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
